f2a194645939e3a7589434a4467799ab5f158b27
Monaliza Loren <83433815+monalizaloren@users.noreply.github.com>
2023-04-24 12:17:08 -0300

    Update sprites.js

--- scripts/sprites.js
+++ scripts/sprites.js
@@ -1,16 +1,13 @@
 
 
-
-var ??? = [ 'imgs/scene/mountains01.png', 'imgs/scene/mountains02.png', 
+var mountainImages = [ 'imgs/scene/mountains01.png', 'imgs/scene/mountains02.png', 
 'imgs/scene/mountains03.png', 'imgs/scene/mountains04.png'];
-
-var ??? = [ 'imgs/scene/cloud01.png', 'imgs/scene/cloud02.png'];
-var ??? = [ 'imgs/blocks/blocks001.png', 'imgs/blocks/blocks002.png', 'imgs/blocks/blocks003.png'];
-var ??? = [ 'imgs/blocks/coin01.png', 'imgs/blocks/coin05.png' ];
-var ??? = [ 'imgs/scene/tube.png' ];
-var ??? = [ 'imgs/scene/platform.png' ];
-var ??? = [ 'imgs/enemy/enemyMushroom01.png','imgs/enemy/enemyMushroom02.png'];
-
+var cloudImages = [ 'imgs/scene/cloud01.png', 'imgs/scene/cloud02.png'];
+var brickImages = [ 'imgs/blocks/blocks001.png', 'imgs/blocks/blocks002.png', 'imgs/blocks/blocks003.png'];
+var coinsImages = [ 'imgs/blocks/coin01.png', 'imgs/blocks/coin05.png' ];
+var pipeImages = [ 'imgs/scene/tube.png' ];
+var platformImages = [ 'imgs/scene/platform.png' ];
+var enemyMushroomImage = [ 'imgs/enemy/enemyMushroom01.png','imgs/enemy/enemyMushroom02.png'];
 
 var spriteNumber={
   mountain: 6,
@@ -22,9 +19,6 @@ var spriteNumber={
 
 }
 
-
-
-// set every sprites configs
 function setSprites(){
   ???
   ???( mountains, mountainImages, spriteNumber.mountain ,1.5, gameConfig.screenX, gameConfig.screenY-35, gameConfig.screenY-35);
@@ -34,4 +28,65 @@ function setSprites(){
   ???( coins, coinsImages, 'shine', spriteNumber.coin, "get", false, 0, gameConfig.screenX, gameConfig.screenY*0.35, gameConfig.screenY*0.75 );
   ???( ene
 }
+
+function setSpriteGroups(){
+  //groups 
+  bricks = ???
+  enemyMushrooms = ???
+  clouds = ???
+  mountains = ???
+  pipes = ???
+  platforms = ???
+  coins =???
+};
+  
+  ???( group, imageArray, spriteNumber, randomPosStartX, randomPosEndX, randomPosStartY, randomPosEndY) {
+  for(var i = 0; i < spriteNumber; i++) {
+  
+    var randomNumber=floor((random()*10)%imageArray.length);
+    var img = loadImage(imageArray[randomNumber]);
+
+    group[i] = createSprite(random(randomPosStartX, randomPosEndX), random(randomPosStartY, randomPosEndY));
+    group[i].addImage(img);
+   
+  }
+};
+ 
+  
+  ???( group, imageArray, animationName, spriteNumber, spriteStatusName, spriteStatusValue,  randomPosStartX, randomPosEndX, randomPosStartY, randomPosEndY) {
+  for(var i = 0; i < spriteNumber; i++) {
+    
+    group[i] = createSprite(random(randomPosStartX, randomPosEndX), random(randomPosStartY, randomPosEndY));
+    group[i].addAnimation(animationName, imageArray[0], imageArray[1]);
+    group[i].scale = 1.5;
+    group[i][spriteStatusName] = spriteStatusValue;
+
+  };
+};
+  
+ ???{
+  img=loadImage('imgs/scene/platform.png');
+  for(i=0;i<70;i++){
+    randomNumber=random();
+    if(randomNumber>0.2){
+      platforms[i]=createSprite(gameConfig.screenX-i*19,gameConfig.screenY-10);
+    }else{
+      platforms[i]=createSprite(random(0,gameConfig.screenX),gameConfig.screenY-10);
+    }
+    platforms[i].addImage(img);
+  };
+};
+  
+
+???{
+  mario=createSprite(gameConfig.startingPointX, gameConfig.startingPointY, gameConfig.startingPoint, 0.30);
+  mario.addAnimation("???",'imgs/mario/mario06.png');
+  mario.addAnimation("???",'imgs/mario/mario01.png','imgs/mario/mario03.png');
+  mario.addAnimation("???",'imgs/mario/mario18.png');
+  mario.addAnimation("???",'imgs/mario/mario05.png');
+  mario.addAnimation("???",'imgs/mario/mario24.png');
+};
+
+
+
 
