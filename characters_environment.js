aa44d4e9ef095a482535111fe32f2d9aa3c46ab2
Monaliza Loren <83433815+monalizaloren@users.noreply.github.com>
2023-04-26 13:27:35 -0300

    Update characters_environment.js

--- characters_environment.js
+++ characters_environment.js
@@ -1,1 +1,27 @@
 
+
+var mario, bricks,clouds,mountains,enemyMushrooms,pipes,platforms,coins;
+
+var control={
+  up: "UP_ARROW",
+  left: 'LEFT_ARROW',
+  right: 'RIGHT_ARROW',
+  revive: 32
+}
+
+
+var gameConfig={
+  status: ?, 
+  initialLifes: ?,
+  moveSpeed: ?,
+  enemyMoveSpeed: ?,
+  gravity: 1,
+  gravityEnemy: ?,
+  jump:?,
+  startingPointX: ?,
+  startingPointY: ?,
+  screenX:?,
+  screenY:?,
+  timeScores: ?,
+  scores: ?
+}
